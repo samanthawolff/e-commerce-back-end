ff9538c39792df316b28be04948b924e4c103012
Samantha Wolff <samanthawolff07@gmail.com>
2022-05-07 21:38:46 -0500

    sync sequelize models to mysql db and working get all categories route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,17 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: {
+      model: Product,
+      attributes: ['product_name']
+    }
+  })
+  .then(categoryData => res.json(categoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
